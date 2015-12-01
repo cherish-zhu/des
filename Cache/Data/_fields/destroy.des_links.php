7a34c33f6f77b858a1444061b04cad741afa6b20
zhushizhen <zhushizhen@hadlink.com>
2015-12-01 17:30:14 +0800

    删除缓存

--- Cache/Data/_fields/destroy.des_links.php
+++
@@ -1,1 +0,0 @@
-a:9:{i:0;s:3:"lid";i:1;s:4:"sort";i:2;s:4:"name";i:3;s:5:"links";i:4;s:5:"order";i:5;s:4:"type";i:6;s:4:"icon";s:3:"_pk";s:3:"lid";s:5:"_type";a:7:{s:3:"lid";s:7:"int(11)";s:4:"sort";s:12:"mediumint(9)";s:4:"name";s:12:"varchar(125)";s:5:"links";s:12:"varchar(255)";s:5:"order";s:7:"int(11)";s:4:"type";s:7:"int(11)";s:4:"icon";s:12:"varchar(255)";}}
--- Cache/Data/_fields/destroy.des_nav.php
+++
@@ -1,1 +0,0 @@
-a:9:{i:0;s:2:"id";i:1;s:9:"parent_id";i:2;s:4:"name";i:3;s:5:"links";i:4;s:4:"sort";i:5;s:6:"status";i:6;s:4:"icon";s:3:"_pk";s:2:"id";s:5:"_type";a:7:{s:2:"id";s:11:"smallint(6)";s:9:"parent_id";s:11:"smallint(6)";s:4:"name";s:12:"varchar(125)";s:5:"links";s:12:"varchar(255)";s:4:"sort";s:7:"int(11)";s:6:"status";s:7:"int(11)";s:4:"icon";s:12:"varchar(255)";}}
