f00d3f67fecb2476cc1633c6ffa05e4c202a56b2
zhushizhen <zhushizhen@hadlink.com>
2015-12-24 16:14:47 +0800

    更新

--- Realize/Admin/View/Index/index.php
+++ Realize/Admin/View/Index/index.php
@@ -78,7 +78,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
+                <!--div class="row">
                     <div class="col-lg-12">
                         <div class="ibox float-e-margins">
                             <div class="ibox-title">
@@ -133,7 +133,7 @@
 
                         </div>
                     </div>
-                </div>
+                </div-->
 
 
                 
