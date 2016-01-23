3fd6833c78ebf040d0496946e871b5d9d593ab45
zhu <1178203757@qq.com>
2016-01-24 01:10:29 +0800

    ..

--- Realize/Content/View/default/Center/list.php
+++ Realize/Content/View/default/Center/list.php
@@ -1,23 +1,70 @@
-<include  file="public:header" />
-<link type="text/css" rel="stylesheet" href="/Realize/Content/View/default/style/css/list.css" />
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Destroy</title>
+<style type="text/css">
+*{padding:0; margin:0 auto}
+body{background-image:url(/Static/background/bg2.jpg);background-size:100%; text-align:center; padding-top:100px}
+</style>
+</head>
 
-<div class="wrap">
-    <div class="left">
-
-        <div class="list">
-            <div class="line"><?php echo $list['name']?></div>
-            <ul>
-                <?php foreach($arr as $list){?>
-                <li><div class="title"><a href="<?php echo URL($list['alias'],$list['cid'])?>"><?php echo $list['title']?></a></div><span class="datetime"><?php echo date("Y-m-d H:i",$list['create_time'])?></span></li>
-                <li style="height:auto; width:640px; margin-left:20px; text-indent:2em; margin-bottom:30px; font-size:13px"><?php echo msubstr(strip_tags($list['center']),0,100)?></li>
-                <?php }?>
-            </ul>
-        </div>
-        
-        <div class="page"><?php echo $page?></div>
-
-    </div>
-    <include  file="public:left" />
-</div>
-
-<include  file="public:footer" />
+<body>
+<table width="960" height="36" border="0" bgcolor="#FFFFFF" cellpadding="0" cellspacing="0">
+  <tr>
+    <?php foreach($nav as $ke => $va){?>
+    <td>&nbsp;&nbsp;&nbsp;<a href="#<?php echo $va['links']?>"><?php echo $va['name']?></a></td>
+    <?php }?>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="960" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td height="36" colspan="2" bgcolor="#FFFFFF">&nbsp;&nbsp;<a href="#">最新文章</a></td>
+    <td width="16" rowspan="7" >&nbsp;</td>
+    <td width="251" rowspan="7" ><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td height="36" bgcolor="#FFFFFF">&nbsp;文章分类</td>
+      </tr>
+      <?php foreach(getCategoryList(1,6) as $get => $li){?>
+      <tr>
+        <td height="36" bgcolor="#FFFFFF">&nbsp;&nbsp;<a href="#"><?php echo $li['name']?></a></td>
+      </tr>
+      <?php }?>
+    </table></td>
+  </tr>
+  <?php foreach(getList(0,6) as $new => $ne){?>
+  <tr>
+    <td width="498" height="36" bgcolor="#FFFFFF">&nbsp;&nbsp;<a href="#"><?php echo $ne['title']?></a></td>
+    <td width="195" bgcolor="#FFFFFF"><?php echo date("Y-m-d H:i",$ne['create_time']);?></td>
+  </tr>
+  <?php }?>
+</table>
+<p>&nbsp;</p>
+<table width="960" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td height="36" bgcolor="#FFFFFF">&nbsp;&nbsp;最新图片</td>
+  </tr>
+  <tr>
+    <td height="220" bgcolor="#FFFFFF"><?php foreach(getPictures(0,8) as $get => $li){?>
+      <img src="<?php echo $li['path']?>" width="200" height="200" style="margin-left:30px; margin-bottom:15px">
+    <?php }?>&nbsp;</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<table width="960" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td height="36" bgcolor="#FFFFFF">&nbsp;&nbsp;友情链接</td>
+  </tr>
+  <tr>
+    <td height="50" bgcolor="#FFFFFF"><?php foreach(getLinks() as $link => $s){
+		echo '<a style="margin-right:30px; margin-left:20px;" href="#'.$s['link'].'">'.$s['name'].'</a>';
+	}
+	?></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
